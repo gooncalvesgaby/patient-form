e367c32c89a27293ddf1e6d787646d66f8d2287d
gooncalvesgaby <gabrielle.goncalves@outlook.com.br>
2024-08-02 01:00:46 -0300

    ajustes na validação

--- src/Components/Form/form.jsx
+++ src/Components/Form/form.jsx
@@ -14,24 +14,31 @@ function Form() {
     function validar() {
         if(email == ""){
             alert('Digite um email válido')
+            return;
         }
         if(patient == ""){
             alert('Informe o nome do paciente')
+            return;
         }
         if(peso == ""){
             alert('Informe o peso do paciente')
+            return;
         }
         if(altura == ""){
             alert('Informe a altura do paciente')
+            return;
         }
         if(name == ""){
             alert('Informe o nome da mãe do paciente')
+            return;
         }
         if(surgery == ""){
             alert('Informe o procedimento a ser realizado')
+            return;
         }
         if(date == ""){
             alert('Informe a data da cirurgia')
+            return;
         }
     }
 
@@ -41,8 +48,8 @@ function Form() {
             <div>
                 <S.Input type="email" placeholder="E-mail do paciente" value={email} onChange={(ev) => setEmail(ev.target.value)}/>
                 <S.Input type="text" placeholder="Digite o nome do paciente" value={patient} onChange={(ev) => setPatient(ev.target.value)} />
-                <S.Input type="number" placeholder="Peso do Paciente" value={peso} onChange={(ev) => setPeso(ev.target.value)} />
-                <S.Input type="number" placeholder="Altura do Paciente" value={altura} onChange={(ev) => setAltura(ev.target.value)} />
+                <S.Input type="text" placeholder="Peso do Paciente" value={peso} onChange={(ev) => setPeso(ev.target.value)} />
+                <S.Input type="text" placeholder="Altura do Paciente" value={altura} onChange={(ev) => setAltura(ev.target.value)} />
                 <S.Input type="text" placeholder="Nome da Mãe" value={name} onChange={(ev) => setName(ev.target.value)} />
                 <S.Input type="text" placeholder="Procedimento a ser realizado" value={surgery} onChange={(ev) => setSurgery(ev.target.value)} />
                 <S.Input type="date" placeholder="Data de nascimento" value={date} onChange={(ev) => setDate(ev.target.value)} />
